2c06efb5a062de344209efde26b047b2c95a198b
Nii_Nartey <highpriest.mn41@gmail.com>
2021-10-20 16:05:24 +0000

    users get calls commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import { Component } from 'react';
 import Navbar from './components/layouts/Navbar'
 import Users from './components/users/Users';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  }
+  
+  async componentDidMount() {
+    this.setState({ loading: true })
+    const res = await axios.get('https://api.github.com/users')
+    console.log(res.data)
+
+    this.setState({ loading: false, users: res.data })
+  }
   render() {
 
     return (
       <div className="App">
         <Navbar title='Github Finder' />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
 
       </div>
